85b881a75eccbcee829a87abee6334b7d4e6b369
Ilya Basin <basinilya@gmail.com>
2015-08-25 13:25:27 +0300

    x

--- cygpipe.c
+++ cygpipe.c
@@ -2,34 +2,49 @@
 #include <sys/cygwin.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#define BUFSZ 100
 
 int main(int argc, char *argv[]) {
-  HANDLE hPipe, hVictim;
+  HANDLE hPipe;
+  int fds[2];
+  char procfs_path[BUFSZ], pipename[BUFSZ] = "";
+
+  pipe(fds);
+
+  sprintf(procfs_path, "/proc/self/fd/%d", fds[0]);
+  readlink(procfs_path, pipename, BUFSZ);
+  close(fds[0]);
+  close(fds[1]);
+  
+  //fprintf(stderr, "pipename: %s\n", pipename);
+
+  fprintf(stderr, "named pipe: %s\n", argv[1]);
+
   hPipe = CreateNamedPipeA(argv[1], PIPE_ACCESS_DUPLEX, PIPE_TYPE_BYTE | PIPE_WAIT, 1, 2048, 2048, 0, NULL);
   if (hPipe == INVALID_HANDLE_VALUE) {
-    fprintf(stderr, "CreateNamedPipe() failed\n");
+    fprintf(stderr, "CreateNamedPipe() failed: %d\n", (int)GetLastError());
     exit(1);
   }
   fprintf(stderr, "CreateNamedPipe() succeeded\n");
-  if (!DuplicateHandle(GetCurrentProcess(), hPipe, GetCurrentProcess(), &hVictim, 0, FALSE, DUPLICATE_SAME_ACCESS)) {
-    fprintf(stderr, "DuplicateHandle() failed");
-    exit(1);
-  }
-  fprintf(stderr, "DuplicateHandle() succeeded\n");
-  //_IFIFO+
+
   if (!ConnectNamedPipe(hPipe, NULL) && GetLastError() != ERROR_PIPE_CONNECTED) {
     fprintf(stderr, "ConnectNamedPipe() failed: %d", (int)GetLastError());
     exit(1);
   }
   fprintf(stderr, "ConnectNamedPipe() succeeded\n");
-  if (3 != cygwin_attach_handle_to_fd(argv[1], 3, hVictim, 1, GENERIC_WRITE | GENERIC_READ)) {
+
+  if (3 != cygwin_attach_handle_to_fd(pipename, 3, hPipe, 1, GENERIC_WRITE | GENERIC_READ)) {
     fprintf(stderr, "cygwin_attach_handle_to_fd() failed");
     exit(1);
   }
   fprintf(stderr, "cygwin_attach_handle_to_fd() succeeded\n");
-  Sleep(100000);
-  return 0;
+
+  Sleep(10000);
+
 #if 0
 #endif
+  return 0;
 }
 
